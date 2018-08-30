db8b1aea1878919f1ea2070781d044e0c01d6080
HabenulaME <habenulame97@gmail.com>
2018-08-30 13:25:59 +0700

    Langkah kedua

---
+++ App.js
@@ -0,0 +1,50 @@
+import React, {Component} from 'react';
+import {
+	Platform, 
+	StyleSheet, 
+	Text, 
+	View, 
+	Image
+} from 'react-native';
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+type Props = {};
+export default class App extends Component<Props> {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Nama : Habenula Mahogra Elfreda</Text>
+        <Text>Kelas: XI RPL 4</Text>
+        <Text>Absen: 13</Text>
+        <Image source={require('./gwganteng.png')} style={{width: 400, height:400}}
+        />
+        
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
